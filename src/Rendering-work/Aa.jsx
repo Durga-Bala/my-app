299cfb1684580eb7b446977746a3fb9c95c48864
DURGA B <durgab.ug22.cs@francisxavier.ac.in>
2024-09-27 09:58:00 +0530

    first commit

---
+++ src/Rendering-work/Aa.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Aa(){
+    return(
+        <>
+        <h1>DURGA</h1>
+        </>
+    );
+}
+export default Aa;
---
+++ src/pages/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      <h2>My Projects</h2>
+      <p>Here are some of the projects I've worked on.</p>
+    </div>
+  );
+};
+
+export default Projects;
